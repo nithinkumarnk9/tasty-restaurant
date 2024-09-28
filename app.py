cec25398ff79ec7657b4df83459d524ae569ed7d
nithin Kumar <nithinkumarnk999@gmail.com>
2024-09-28 23:20:03 +0530

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ app= Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-
+@app.route("/gallery.html")
+def gallery():
+    return render_template("gallery.html")
+@app.route("/menu.html")
+def menu():
+    return render_template("menu.html")
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
 if __name__=='__main__':
     app.run(debug=True)
